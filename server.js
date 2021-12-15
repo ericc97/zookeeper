d920a305629b67c36b4ef8c419e3a273c4c71b9c
Eric Coleman <ericcoleman9217@gmail.com>
2021-12-15 09:43:42 -0600

    added get and post functionality, along with post validation

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const {animals} = require('./data/animals.json');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+// "mount" middleware 
+// takes incoming POST data and converts it to key/ value pairs
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -50,6 +59,35 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false; 
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false
+    }
+    return true;
+}
+
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
 
 
 
@@ -68,6 +106,22 @@ app.get('/api/animals/:id', (req, res) => {
     }else {
         res.send(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 404 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal you are trying to add is not properly formatted!. Try that shit again');
+    } else {
+        // add animal to JSON file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
 });
 
 
